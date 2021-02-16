9fcf040d8ed1a1c7d97bdf0043edf190e5369efe
jwar663 <32050015+jwar663@users.noreply.github.com>
2021-02-16 16:42:23 +1300

    Updated formatting threshold components

--- qupath-gui-fx/src/main/java/qupath/lib/gui/commands/DuplicateMatrixCommand.java
+++ qupath-gui-fx/src/main/java/qupath/lib/gui/commands/DuplicateMatrixCommand.java
@@ -25,20 +25,15 @@ package qupath.lib.gui.commands;
 
 import java.awt.image.BufferedImage;
 import java.io.IOException;
-import java.text.DecimalFormat;
 
 import javafx.embed.swing.SwingFXUtils;
-import javafx.geometry.Orientation;
-import javafx.geometry.Pos;
+import javafx.geometry.*;
 import javafx.scene.control.*;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
 import javafx.scene.text.Text;
 import javafx.stage.Modality;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import qupath.lib.common.ConcatChannelsABI;
@@ -76,9 +71,13 @@ public class DuplicateMatrixCommand implements Runnable {
     }
 
     protected Stage createDialog() throws IOException, NullPointerException {
+
+        Stage dialog = new Stage();
+        dialog.initOwner(qupath.getStage());
+        dialog.setTitle("Duplicate Matrix");
+
         viewer = qupath.getViewer();
         imageData = qupath.getImageData();
-        System.out.println(imageData);
         if(imageData == null) {
             Stage error = new Stage();
             error.initModality(Modality.WINDOW_MODAL);
@@ -103,15 +102,38 @@ public class DuplicateMatrixCommand implements Runnable {
 
         //larger panes
         Pane pane = new Pane();
+        pane.setPrefSize(900.0, 800.0);
+
         BorderPane overallPane = new BorderPane();
-        Stage dialog = new Stage();
-        dialog.initOwner(qupath.getStage());
-        dialog.setTitle("Duplicate Matrix");
+        overallPane.setPrefSize(880.0, 780.0);
+        overallPane.setLayoutX(10.0);
+        overallPane.setLayoutY(10.0);
+        pane.getChildren().add(overallPane);
+
+        GridPane thresholdPane = new GridPane();
+        thresholdPane.setPrefSize(880.0, 25.0);
+        //thresholdPane.setAlignment(Pos.CENTER);
+        ColumnConstraints labelColumn = new ColumnConstraints(750.0, 750.0, 750.0);
+        ColumnConstraints fieldColumn = new ColumnConstraints(80.0, 80.0, 80.0);
+        ColumnConstraints buttonColumn = new ColumnConstraints(50.0, 50.0, 50.0);
+        RowConstraints rowConstraints = new RowConstraints(25.0, 25.0, 25.0);
 
         //Threshold Part
         Label thresholdLabel = new Label("Please enter the correct threshold value:");
+        thresholdLabel.setPrefHeight(25.0);
+        thresholdLabel.setMinHeight(25.0);
+        thresholdLabel.setMaxHeight(25.0);
+        GridPane.setHalignment(thresholdLabel, HPos.CENTER);
         TextField thresholdTextField = new TextField("0.90");
+        thresholdTextField.setPrefSize(40.0, 25.0);
+        thresholdTextField.setMinSize(40.0, 25.0);
+        thresholdTextField.setMaxSize(40.0, 25.0);
+        GridPane.setHalignment(thresholdTextField, HPos.CENTER);
         Button thresholdConfirm = new Button("OK");
+        thresholdConfirm.setPrefSize(40.0, 25.0);
+        thresholdConfirm.setMinSize(40.0, 25.0);
+        thresholdConfirm.setMaxSize(40.0, 25.0);
+        GridPane.setHalignment(thresholdConfirm, HPos.CENTER);
         thresholdConfirm.setOnAction(event -> {
             String thresholdValue = thresholdTextField.getText();
             try{
@@ -125,71 +147,74 @@ public class DuplicateMatrixCommand implements Runnable {
                 System.out.println("Exception: " + e);
             }
         });
-        GridPane thresholdGrid = new GridPane();
-        thresholdGrid.add(thresholdLabel, 0, 0);
-        thresholdGrid.add(thresholdTextField, 1, 0);
-        thresholdGrid.add(thresholdConfirm, 2, 0);
-        overallPane.setTop(thresholdGrid);
+        thresholdPane.add(thresholdLabel, 0, 0);
+        thresholdPane.add(thresholdTextField, 1, 0);
+        thresholdPane.add(thresholdConfirm, 2, 0);
+        thresholdPane.getColumnConstraints().addAll(labelColumn, fieldColumn, buttonColumn);
+        thresholdPane.getRowConstraints().add(rowConstraints);
+        overallPane.setTop(thresholdPane);
+
+
 
         //preview image section
-        HBox imageHBox = new HBox(10);
-        VBox image1VBox = new VBox();
-        VBox image2VBox = new VBox();
-        Pane imagePane1 = new Pane();
-        Pane imagePane2 = new Pane();
-        imageHBox.getChildren().addAll(image1VBox, image2VBox);
-        Label image1Label = new Label("Image 1");
-        Label image2Label = new Label("Image 2");
-        ImageView imageView1 = new ImageView();
-        ImageView imageView2 = new ImageView();
-        imagePane1.getChildren().add(imageView1);
-        imagePane2.getChildren().add(imageView2);
-        image1VBox.getChildren().addAll(image1Label, imagePane1);
-        image2VBox.getChildren().addAll(image2Label, imagePane2);
-        overallPane.setBottom(imageHBox);
+//        HBox imageHBox = new HBox(10);
+//        VBox image1VBox = new VBox();
+//        VBox image2VBox = new VBox();
+//        Pane imagePane1 = new Pane();
+//        Pane imagePane2 = new Pane();
+//        imageHBox.getChildren().addAll(image1VBox, image2VBox);
+//        Label image1Label = new Label("Image 1");
+//        Label image2Label = new Label("Image 2");
+//        ImageView imageView1 = new ImageView();
+//        ImageView imageView2 = new ImageView();
+//        imagePane1.getChildren().add(imageView1);
+//        imagePane2.getChildren().add(imageView2);
+//        image1VBox.getChildren().addAll(image1Label, imagePane1);
+//        image2VBox.getChildren().addAll(image2Label, imagePane2);
+//        overallPane.setBottom(imageHBox);
+//
+//        //matrix part
+//        BorderPane matrixPane = new BorderPane();
+//        GridPane matrix = new GridPane();
+//        GridPane labelVertical = new GridPane();
+//        GridPane labelHorizontal = new GridPane();;
+//        for(int i = 0; i < size; i++) {
+//            for(int j = 0; j < size; j++) {
+//                if(j == 0 && i== 0) {
+//                    Label tempLabel1 = new Label(Integer.toString(j + 1));
+//                    Label tempLabel2 = new Label(Integer.toString(i + 1));
+//                    labelHorizontal.add(tempLabel1, i, j);
+//                    labelVertical.add(tempLabel2, i, j);
+//                }
+//                else if(i == 0) {
+//                    Label tempLabel = new Label(Integer.toString(j + 1));
+//                    labelHorizontal.add(tempLabel, i, j);
+//                }
+//                else if(j == 0) {
+//                    Label tempLabel = new Label(Integer.toString(i + 1));
+//                    labelVertical.add(tempLabel, i, j);
+//                }
+//                String tempString = String.format("%.2f", duplicateMatrix[i][j]);
+//                //set buttons to be the corresponding matrix
+//                Button tempButton = new Button(tempString);
+//                int tempI = i + 1;
+//                int tempJ = j + 1;
+//                tempButton.setOnAction(e -> {
+//                    //set the correct images depending on button click
+//                    image1Label.setText("Channel " + tempI);
+//                    image2Label.setText("Channel " + tempJ);
+//                    if(imageData != null) {
+//                        imageView1.setImage(SwingFXUtils.toFXImage(ConcatChannelsABI.singleChannelImage(imageData, tempI), null));
+//                        imageView2.setImage(SwingFXUtils.toFXImage(ConcatChannelsABI.singleChannelImage(imageData, tempJ), null));
+//                    }
+//                });
+//                matrix.add(tempButton, i, j);
+//            }
+//        }
+//        overallPane.setCenter(matrixPane);
 
-        //matrix part
-        BorderPane matrixPane = new BorderPane();
-        GridPane matrix = new GridPane();
-        GridPane labelVertical = new GridPane();
-        GridPane labelHorizontal = new GridPane();;
-        for(int i = 0; i < size; i++) {
-            for(int j = 0; j < size; j++) {
-                if(j == 0 && i== 0) {
-                    Label tempLabel1 = new Label(Integer.toString(j + 1));
-                    Label tempLabel2 = new Label(Integer.toString(i + 1));
-                    labelHorizontal.add(tempLabel1, i, j);
-                    labelVertical.add(tempLabel2, i, j);
-                }
-                else if(i == 0) {
-                    Label tempLabel = new Label(Integer.toString(j + 1));
-                    labelHorizontal.add(tempLabel, i, j);
-                }
-                else if(j == 0) {
-                    Label tempLabel = new Label(Integer.toString(i + 1));
-                    labelVertical.add(tempLabel, i, j);
-                }
-                String tempString = String.format("%.2f", duplicateMatrix[i][j]);
-                //set buttons to be the corresponding matrix
-                Button tempButton = new Button(tempString);
-                int tempI = i + 1;
-                int tempJ = j + 1;
-                tempButton.setOnAction(e -> {
-                    //set the correct images depending on button click
-                    image1Label.setText("Channel " + tempI);
-                    image2Label.setText("Channel " + tempJ);
-                    if(imageData != null) {
-                        imageView1.setImage(SwingFXUtils.toFXImage(ConcatChannelsABI.singleChannelImage(imageData, tempI), null));
-                        imageView2.setImage(SwingFXUtils.toFXImage(ConcatChannelsABI.singleChannelImage(imageData, tempJ), null));
-                    }
-                });
-                matrix.add(tempButton, i, j);
-            }
-        }
-        overallPane.setCenter(matrixPane);
 
-
-        pane.getChildren().add(overallPane);
+        //pane.getChildren().add(overallPane);
 
         Scene scene = new Scene(pane, 900, 800);
         dialog.setScene(scene);
