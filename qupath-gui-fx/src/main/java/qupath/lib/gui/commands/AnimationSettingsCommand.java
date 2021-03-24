94fc8fc18b4ac6e6f17d2d3aba3da46391f57ba2
jwar663 <32050015+jwar663@users.noreply.github.com>
2021-03-24 16:30:06 +1300

    Fix layout for animation settings

--- qupath-gui-fx/src/main/java/qupath/lib/gui/commands/AnimationSettingsCommand.java
+++ qupath-gui-fx/src/main/java/qupath/lib/gui/commands/AnimationSettingsCommand.java
@@ -23,37 +23,15 @@
 
 package qupath.lib.gui.commands;
 
-import javafx.embed.swing.SwingFXUtils;
-import javafx.event.EventHandler;
-import javafx.geometry.HPos;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
-import javafx.geometry.Side;
+
+import javafx.geometry.*;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
-import javafx.scene.image.ImageView;
-import javafx.scene.input.ScrollEvent;
 import javafx.scene.layout.*;
-import javafx.scene.text.Text;
-import javafx.stage.Modality;
 import javafx.stage.Stage;
-import qupath.lib.common.ConcatChannelsABI;
-import qupath.lib.common.GeneralTools;
 import qupath.lib.gui.QuPathGUI;
-import qupath.lib.gui.viewer.QuPathViewer;
-import qupath.lib.images.ImageData;
-import qupath.lib.images.servers.ImageServer;
-import qupath.lib.images.writers.ImageWriter;
-import qupath.lib.images.writers.ImageWriterTools;
-import qupath.lib.regions.RegionRequest;
-
-import java.awt.image.BufferedImage;
-import java.io.File;
 import java.io.IOException;
-import java.net.URI;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
+
 
 /**
  * Command to show a Duplicate Matrix widget to preview and decide which threshold
@@ -65,72 +43,16 @@ import java.util.List;
 public class AnimationSettingsCommand implements Runnable {
 
     private QuPathGUI qupath;
-    private QuPathViewer viewer;
 
     private Stage dialog;
 
-    private float[][] duplicateMatrix;
-    private BufferedImage img;
-    private Double confirmDouble = 0.0;
+    private double BASE_WIDTH = 60;
+    private double BASE_HEIGHT = 25;
 
-    public ImageData<BufferedImage> imageData;
+    private double PADDING = 5;
 
-    //CONSTANT MACROS
-    private static final double BUTTON_WIDTH = 42.0;
-    private static final double BUTTON_LABEL_HEIGHT = 25.0;
-    private static final double SCROLL_BAR_FONT_SIZE = 12.0;
-    private static final double TAB_SIZE = 30.0;
-
-
-    //MAX/PREF
-
-    //OVERALL MACROS MAX/PREF
-    private static final double OVERALL_WIDTH = 900.0;
-    private static final double OVERALL_HEIGHT = 700.0;
-
-    //THRESHOLD MACROS MAX/PREF
-    private static final double THRESHOLD_WIDTH = OVERALL_WIDTH - 20.0;
-    private static final double THRESHOLD_HEIGHT = 25.0;
-
-    private static final double THRESHOLD_BUTTONS_WIDTH = 80.0;
-    private static final double THRESHOLD_TEXT_FIELD_WIDTH = 40.0;
-    private static final double THRESHOLD_LABEL_WIDTH = 620.0;
-
-    private static final double THRESHOLD_FIELD_COLUMN = THRESHOLD_TEXT_FIELD_WIDTH * 2;
-    private static final double THRESHOLD_BUTTON_COLUMN = THRESHOLD_BUTTONS_WIDTH + 10.0;
-
-    //MATRIX MACROS MAX/PREF
-    private static final double MATRIX_BORDER_WIDTH = OVERALL_WIDTH - 20.0;
-    private static final double MATRIX_BORDER_HEIGHT = 329.0;
-    private static final double MATRIX_BORDER_WIDTH_PREF = BUTTON_LABEL_HEIGHT + BUTTON_WIDTH;
-    private static final double MATRIX_BORDER_HEIGHT_PREF = BUTTON_LABEL_HEIGHT * 2;
-
-    private static final double MATRIX_LABELS_VERTICAL_WIDTH = 25.0;
-    private static final double MATRIX_LABELS_VERTICAL_HEIGHT = MATRIX_BORDER_HEIGHT - BUTTON_LABEL_HEIGHT - SCROLL_BAR_FONT_SIZE - 2;
-    private static final double MATRIX_LABELS_HORIZONTAL_WIDTH = MATRIX_BORDER_WIDTH - SCROLL_BAR_FONT_SIZE - 2;
-    private static final double MATRIX_LABELS_HORIZONTAL_HEIGHT = 25.0;
-    private static final double MATRIX_SCROLL_HEIGHT = MATRIX_BORDER_HEIGHT - MATRIX_LABELS_HORIZONTAL_HEIGHT;
-    private static final double MATRIX_SCROLL_WIDTH = MATRIX_BORDER_WIDTH - MATRIX_LABELS_VERTICAL_WIDTH;
-
-    //IMAGE MACROS MAX/PREF
-    private static final double TAB_WIDTH = OVERALL_WIDTH - 20.0;
-    private static final double TAB_HEIGHT = 306.0;
-
-    private static final double IMAGE_HBOX_WIDTH = TAB_WIDTH - TAB_SIZE - 10.0;
-    private static final double IMAGE_HBOX_HEIGHT = TAB_HEIGHT;
-
-    private static final double IMAGE_VBOX_WIDTH = (IMAGE_HBOX_WIDTH - 10.0)/2;
-    private static final double IMAGE_VBOX_HEIGHT = TAB_HEIGHT;
-
-    private static final double IMAGE_LABEL_WIDTH = IMAGE_VBOX_WIDTH;
-    private static final double IMAGE_LABEL_HEIGHT = 25.0;
-
-    private static final double IMAGE_WIDTH = IMAGE_VBOX_WIDTH;
-    private static final double IMAGE_HEIGHT = IMAGE_VBOX_HEIGHT - IMAGE_LABEL_HEIGHT - 25;
-
-    private static final String START_THRESHOLD = "0.90";
-
-    String thresholdValue = START_THRESHOLD;
+    private double OVERALL_WIDTH = ((BASE_WIDTH + PADDING) * 2) + PADDING;
+    private double OVERALL_HEIGHT = (5 * (BASE_HEIGHT + PADDING)) + PADDING;
 
 
     /**
@@ -139,30 +61,72 @@ public class AnimationSettingsCommand implements Runnable {
      */
     public AnimationSettingsCommand(final QuPathGUI qupath) {
         this.qupath = qupath;
-        this.viewer = qupath.getViewer();
     }
 
     protected Stage createDialog() throws IOException, NullPointerException {
 
         Stage dialog = new Stage();
         dialog.initOwner(qupath.getStage());
-        dialog.setTitle("Duplicate Matrix");
+        dialog.setTitle("Animation Settings");
 
         GridPane overallPane = new GridPane();
+        overallPane.setMaxSize(OVERALL_WIDTH - (PADDING * 2), OVERALL_HEIGHT - (PADDING * 2));
+        overallPane.setMinSize(OVERALL_WIDTH - (PADDING * 2), OVERALL_HEIGHT - (PADDING * 2));
+        overallPane.setPrefSize(OVERALL_WIDTH - (PADDING * 2), OVERALL_HEIGHT - (PADDING * 2));
+        overallPane.setHgap(PADDING);
+        overallPane.setVgap(PADDING);
+        overallPane.setPadding(new Insets(PADDING));
 
         ToggleButton singleStackToggle = new ToggleButton("Single");
+        GridPane.setHalignment(singleStackToggle, HPos.CENTER);
+        GridPane.setValignment(singleStackToggle, VPos.CENTER);
+        singleStackToggle.setMaxSize(OVERALL_WIDTH - (PADDING*2), BASE_HEIGHT);
+        singleStackToggle.setMinSize(OVERALL_WIDTH - (PADDING*2), BASE_HEIGHT);
+        singleStackToggle.setPrefSize(OVERALL_WIDTH - (PADDING*2), BASE_HEIGHT);
+
 
         Label delayLabel = new Label("Delay(ms)");
+        GridPane.setHalignment(delayLabel, HPos.CENTER);
+        GridPane.setValignment(delayLabel, VPos.CENTER);
+        delayLabel.setMaxSize(BASE_WIDTH, BASE_HEIGHT);
+        delayLabel.setMinSize(BASE_WIDTH, BASE_HEIGHT);
+        delayLabel.setPrefSize(BASE_WIDTH, BASE_HEIGHT);
 
         TextField delayField = new TextField("250");
+        GridPane.setHalignment(delayField, HPos.CENTER);
+        GridPane.setValignment(delayField, VPos.CENTER);
+        delayField.setMaxSize(BASE_WIDTH, BASE_HEIGHT);
+        delayField.setMinSize(BASE_WIDTH, BASE_HEIGHT);
+        delayField.setPrefSize(BASE_WIDTH, BASE_HEIGHT);
+
 
         Button chooseFolderButton = new Button("Choose Folder");
+        GridPane.setHalignment(chooseFolderButton, HPos.CENTER);
+        GridPane.setValignment(chooseFolderButton, VPos.CENTER);
+        chooseFolderButton.setMaxSize(OVERALL_WIDTH - (PADDING*2), BASE_HEIGHT);
+        chooseFolderButton.setMinSize(OVERALL_WIDTH - (PADDING*2), BASE_HEIGHT);
+        chooseFolderButton.setPrefSize(OVERALL_WIDTH - (PADDING*2), BASE_HEIGHT);
 
         Label exportLabel = new Label("Export to: ");
+        GridPane.setHalignment(exportLabel, HPos.CENTER);
+        GridPane.setValignment(exportLabel, VPos.CENTER);
+        exportLabel.setMaxSize(OVERALL_WIDTH - (PADDING*2), BASE_HEIGHT);
+        exportLabel.setMinSize(OVERALL_WIDTH - (PADDING*2), BASE_HEIGHT);
+        exportLabel.setPrefSize(OVERALL_WIDTH - (PADDING*2), BASE_HEIGHT);
 
         Button confirmButton = new Button("Confirm");
+        GridPane.setHalignment(confirmButton, HPos.CENTER);
+        GridPane.setValignment(confirmButton, VPos.CENTER);
+        confirmButton.setMaxSize(BASE_WIDTH, BASE_HEIGHT);
+        confirmButton.setMinSize(BASE_WIDTH, BASE_HEIGHT);
+        confirmButton.setPrefSize(BASE_WIDTH, BASE_HEIGHT);
 
         Button cancelButton = new Button("Cancel");
+        GridPane.setHalignment(cancelButton, HPos.CENTER);
+        GridPane.setValignment(cancelButton, VPos.CENTER);
+        cancelButton.setMaxSize(BASE_WIDTH, BASE_HEIGHT);
+        cancelButton.setMinSize(BASE_WIDTH, BASE_HEIGHT);
+        cancelButton.setPrefSize(BASE_WIDTH, BASE_HEIGHT);
 
         overallPane.add(singleStackToggle, 0, 0, 2, 1);
         overallPane.add(delayLabel, 0, 1, 1, 1);
@@ -174,10 +138,10 @@ public class AnimationSettingsCommand implements Runnable {
 
         Scene scene = new Scene(overallPane, OVERALL_WIDTH, OVERALL_HEIGHT);
         dialog.setScene(scene);
-        dialog.setMinWidth(OVERALL_WIDTH);
-        dialog.setMinHeight(OVERALL_HEIGHT);
-        dialog.setMaxWidth(OVERALL_WIDTH);
-        dialog.setMaxHeight(OVERALL_HEIGHT);
+//        dialog.setMinWidth(OVERALL_WIDTH);
+//        dialog.setMinHeight(OVERALL_HEIGHT);
+//        dialog.setMaxWidth(OVERALL_WIDTH);
+//        dialog.setMaxHeight(OVERALL_HEIGHT);
 
         return dialog;
     }
