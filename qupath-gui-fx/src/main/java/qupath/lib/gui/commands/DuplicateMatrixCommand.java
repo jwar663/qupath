fe6bb6bc03328c2a741ff68b365060fb43b987e5
jwar663 <32050015+jwar663@users.noreply.github.com>
2021-06-02 12:02:30 +1200

    Create base for new dialog and allow for number of channels to be passed to other dialogs

--- qupath-gui-fx/src/main/java/qupath/lib/gui/commands/DuplicateMatrixCommand.java
+++ qupath-gui-fx/src/main/java/qupath/lib/gui/commands/DuplicateMatrixCommand.java
@@ -692,6 +692,11 @@ public class DuplicateMatrixCommand implements Runnable {
         unmixingButton.setMinSize(THRESHOLD_BUTTONS_WIDTH, BUTTON_LABEL_HEIGHT);
         unmixingButton.setMaxSize(THRESHOLD_BUTTONS_WIDTH, BUTTON_LABEL_HEIGHT);
 
+        int[] numberOfChannels = new int[7];
+        for(int i = 0; i < numberOfChannels.length; i++) {
+            numberOfChannels[i] = 0;
+        }
+
         unmixingButton.setOnAction(e -> {
 
                 File file = Dialogs.promptForFile("Select indirect data csv file", null, null);
@@ -709,7 +714,7 @@ public class DuplicateMatrixCommand implements Runnable {
 
             Stage unmixDialog;
 
-            unmixDialog = createUnmixingDialog(imageData, dialog);
+            unmixDialog = createUnmixingDialog(imageData, numberOfChannels, dialog);
             unmixDialog.initOwner(dialog);
             unmixDialog.initModality(Modality.WINDOW_MODAL);
             unmixDialog.showAndWait();
@@ -1128,7 +1133,134 @@ public class DuplicateMatrixCommand implements Runnable {
         return previewDialog;
     }
 
-    protected Stage createUnmixingDialog(ImageData<BufferedImage> imageData, Stage duplicateDialog) throws  NullPointerException {
+    protected Stage createUnmixingDialog(ImageData<BufferedImage> imageData, int[] numberOfChannels, Stage duplicateDialog) throws  NullPointerException {
+
+        Stage previewDialog = new Stage();
+        previewDialog.setTitle("Unmix Options");
+
+        Pane overallPane = createOverallPane();
+
+        //larger panes
+        GridPane gridPane = new GridPane();
+        gridPane.setMaxSize(400.0, 410.0);
+        gridPane.setPrefSize(400.0, 410.0);
+        gridPane.setMinSize(400.0, 410.0);
+        overallPane.getChildren().add(gridPane);
+
+        Label DAPILabel = createUnmixLabel("DAPI");
+        DAPILabel.setAlignment(Pos.CENTER);
+        ChoiceBox<Integer> DAPIBox = createUnmixChoiceBox(9);
+        DAPIBox.setOnAction(e -> {
+            numberOfChannels[0] = DAPIBox.getValue();
+        });
+        gridPane.add(DAPILabel, 0,1);
+        gridPane.add(DAPIBox, 0, 2);
+
+        Label Opal780Label = createUnmixLabel("Opal780");
+        Opal780Label.setAlignment(Pos.CENTER);
+        ChoiceBox<Integer> Opal780Box = createUnmixChoiceBox(2);
+        Opal780Box.setOnAction(e -> {
+            numberOfChannels[1] = Opal780Box.getValue();
+        });
+        gridPane.add(Opal780Label, 1,1);
+        gridPane.add(Opal780Box, 1, 2);
+
+        Label Opal480Label = createUnmixLabel("Opal480");
+        Opal480Label.setAlignment(Pos.CENTER);
+        ChoiceBox<Integer> Opal480Box = createUnmixChoiceBox(6);
+        Opal480Box.setOnAction(e -> {
+            numberOfChannels[2] = Opal480Box.getValue();
+        });
+        gridPane.add(Opal480Label, 2,1);
+        gridPane.add(Opal480Box, 2, 2);
+
+        Label Opal690Label = createUnmixLabel("Opal690");
+        Opal690Label.setAlignment(Pos.CENTER);
+        ChoiceBox<Integer> Opal690Box = createUnmixChoiceBox(3);
+        Opal690Box.setOnAction(e -> {
+            numberOfChannels[3] = Opal690Box.getValue();
+        });
+        gridPane.add(Opal690Label, 0,3);
+        gridPane.add(Opal690Box, 0, 4);
+
+        Label FITCLabel = createUnmixLabel("FITC");
+        FITCLabel.setAlignment(Pos.CENTER);
+        ChoiceBox<Integer> FITCBox = createUnmixChoiceBox(9);
+        FITCBox.setOnAction(e -> {
+            numberOfChannels[4] = FITCBox.getValue();
+        });
+        gridPane.add(FITCLabel, 1,3);
+        gridPane.add(FITCBox, 1, 4);
+
+        Label Cy3Label = createUnmixLabel("Cy3");
+        Cy3Label.setAlignment(Pos.CENTER);
+        ChoiceBox<Integer> Cy3Box = createUnmixChoiceBox(7);
+        Cy3Box.setOnAction(e -> {
+            numberOfChannels[5] = Cy3Box.getValue();
+        });
+        gridPane.add(Cy3Label, 2,3);
+        gridPane.add(Cy3Box, 2, 4);
+
+        Label TexasRedLabel = createUnmixLabel("TexasRed");
+        TexasRedLabel.setAlignment(Pos.CENTER);
+        ChoiceBox<Integer> TexasRedBox = createUnmixChoiceBox(7);
+        TexasRedBox.setOnAction(e -> {
+            numberOfChannels[6] = TexasRedBox.getValue();
+        });
+        gridPane.add(TexasRedLabel, 1,5);
+        gridPane.add(TexasRedBox, 1, 6);
+
+        Button nextButton = new Button("Next");
+        nextButton.setPrefSize(100.0, 50.0);
+        nextButton.setMinSize(100.0, 50.0);
+        nextButton.setMaxSize(100.0, 50.0);
+        gridPane.add(nextButton, 2, 5, 1, 2);
+
+
+        nextButton.setOnAction(e -> {
+            //Go to the next option
+            for(int i = 0; i < numberOfChannels.length; i++) {
+                System.out.println(numberOfChannels[i]);
+            }
+        });
+
+        ColumnConstraints column1 = new ColumnConstraints(134.0, 134.0, 134.0);
+        column1.setHalignment(HPos.CENTER);
+        ColumnConstraints column2 = new ColumnConstraints(134.0, 134.0, 134.0);
+        column2.setHalignment(HPos.CENTER);
+        ColumnConstraints column3 = new ColumnConstraints(134.0, 134.0, 134.0);
+        column3.setHalignment(HPos.CENTER);
+
+        RowConstraints row1 = new RowConstraints(58.0, 58.0, 58.0);
+        RowConstraints row2 = new RowConstraints(58.0, 58.0, 58.0);
+        row2.setValignment(VPos.BOTTOM);
+        RowConstraints row3 = new RowConstraints(58.0, 58.0, 58.0);
+        row3.setValignment(VPos.TOP);
+        RowConstraints row4 = new RowConstraints(58.0, 58.0, 58.0);
+        row4.setValignment(VPos.BOTTOM);
+        RowConstraints row5 = new RowConstraints(58.0, 58.0, 58.0);
+        row5.setValignment(VPos.TOP);
+        RowConstraints row6 = new RowConstraints(58.0, 58.0, 58.0);
+        row6.setValignment(VPos.BOTTOM);
+        RowConstraints row7 = new RowConstraints(58.0, 58.0, 58.0);
+        row7.setValignment(VPos.TOP);
+
+        gridPane.getColumnConstraints().addAll(column1, column2, column3);
+        gridPane.getRowConstraints().addAll(row1, row2, row3, row4, row5, row6, row7);
+
+
+
+        Scene scene = new Scene(overallPane, 402.0, 406.0);
+        previewDialog.setScene(scene);
+        previewDialog.setMinWidth(402.0);
+        previewDialog.setMinHeight(406.0);
+        previewDialog.setMaxWidth(402.0);
+        previewDialog.setMaxHeight(406.0);
+
+        return previewDialog;
+    }
+
+    protected Stage createChannelSelectionDialog(ImageData<BufferedImage> imageData, Stage duplicateDialog) throws  NullPointerException {
 
         Stage previewDialog = new Stage();
         previewDialog.setTitle("Unmix Options");
